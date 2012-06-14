f61c08f734c1ffeacaeaa5165fb66a8ab30398c7
Tobenna Peter Igwe <tpigwe@gmail.com>
2012-06-14 16:59:47 +0100

    Added LH-path conversion

--- README.md
+++ README.md
@@ -5,4 +5,13 @@ The Lemke Howson algorithm simulated with the use of covering vectors.
 
 Changes:
 ============
-- Added functionality to accept decimal input as part of the matrix
+- Added _'./path/path'_ program, which converts the given Lemke-Howson path to
+  an equivalent path for Lemke's algorithm on the LCP. This takes as
+  input, the size of the game (m and n), followed by the sequence of
+  labels. See _'./path/path-input'_ for an example.
+- Added _'./path/gen-path'_, which uses the path program to generate an input
+  for the _'./inlemkehowson'_ program. This program assumes that the path
+  in the file _'./path/path-input'_ is the path for the game in _'./sample-input'_.
+  Once the path has been converted, it stores it in _'./path/path-output'_ and 
+  concatenates it with _'./sample-input'_, storing the resulting output in
+  _'./path/lh-input'_ which is then passed as input to _'./inlh -i'_
---
+++ path/path.c
@@ -0,0 +1,156 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define Z(i) (i)
+#define W(i) (i + ndim)
+#define MAXSTR 100
+
+int ndim;
+int m, n;
+
+int* basic;
+int count;
+
+struct node
+{
+	int value;
+	struct node* next;
+}
+node;
+
+struct node* root;
+
+void notimpl (char *info)
+{
+    fflush(stdout);
+    fprintf(stderr, "Program terminated with error. %s\n", info);
+    exit(1);
+}
+
+void readconf (const char *s)
+{
+    int i, len = strlen(s);
+    char a[MAXSTR];
+    for (i=0; i<len; i++)
+        {
+        if (scanf("%1s", &a[i])==EOF)
+            /* make sure something is in  a  for error report       */
+            a[i] = '\0';
+        if (a[i] != s[i])
+            /* the chars in  a  from stdin do not match those in s  */
+            {
+            fprintf(stderr, "\"%s\"  required from input, found \"%s\"\n",
+                    s, a);
+            notimpl("");
+            }
+        }
+}
+
+void printVariable(int var)
+{
+	if(var <= ndim)
+	{
+		printf("z%d ", var);
+	}
+	else
+	{
+		printf("w%d ", var-ndim);
+	}
+}
+
+void dump()
+{
+	printf("Basic:\n");
+	int i;
+	for(i = 0; i < ndim; i++)
+	{
+		printVariable(basic[i]);
+	}
+	printf("\n");
+}
+
+void pivot(int i)
+{	
+	printVariable(basic[i+1]);
+	printf("\n");
+	if(basic[i+1] == W(i+2))
+	{
+		basic[i+1] = Z(i+2);
+	}
+	else
+	{
+		basic[i+1] = W(i+2);
+	}
+}
+
+void init(int var)
+{
+	if((root->next->value) <= m)
+	{
+		printf("w2\n");
+		pivot(var);
+		printf("w1\n");
+	}
+	else
+	{
+		printf("w1\n");
+		pivot(var);
+		printf("w2\n");
+	}
+}
+
+int main(int argc, char** argv)
+{
+	readconf("m=");
+	scanf("%d", &m);
+	readconf("n=");
+	scanf("%d", &n);
+	readconf("labels:");
+	ndim = 2+m+n;
+	
+	basic = (int*)malloc(ndim * sizeof(int));
+	
+	int i;
+	for(i = 0; i < 2+m+n; i++)
+	{
+		basic[i] = W(i+1);
+	}
+	
+	int var;
+	count = 0;
+	
+	root = malloc(sizeof(node));
+	struct node* next = root;
+	struct node* prev = root;
+	while(scanf("%d", &var) != EOF)
+	{
+		next->value = var;
+		next->next = malloc(sizeof(node));
+		prev = next;
+		next = next->next;
+	}
+	free(prev->next);
+	prev->next = NULL;
+	
+	struct node* sec = root->next;
+	int tmp = sec->value;
+	sec->value = prev->value;
+	prev->value = tmp;
+	
+	next = root;
+	while(next != NULL)
+	{
+		if(count == 0)
+			init(next->value);
+		else
+			pivot(next->value);
+		count++;
+		
+		next = next->next;
+	}
+	printf("z0\n");
+	
+	free(basic);
+	return 0;
+}
